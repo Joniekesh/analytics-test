87e6c488d6bb5ec7c9892658a444f267247182d4
Okoro John Darlington <joniedev101@gmail.com>
2024-04-03 09:20:24 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
+import { useEffect, useState } from "react";
+import ReactGA from "react-ga4";
+
 const App = () => {
+  const [analyticsData, setAnalyticsData] = useState(null);
+
+  useEffect(() => {
+    ReactGA.initialize("G-08SGRJZ6RY");
+
+    ReactGA.send("pageview", {
+      page: window.location.pathname + window.location.search,
+    });
+
+    const data = ReactGA.ga("getData");
+    setAnalyticsData(data);
+  }, []);
+
+  useEffect(() => {
+    ReactGA.event({
+      category: "Category name",
+      action: "Action name",
+    });
+
+    ReactGA.send("pageview", { page: "/" });
+
+    const data = ReactGA.ga("getData");
+    setAnalyticsData(data);
+  }, []);
+
+  console.log(analyticsData);
+
   return (
     <div className="app">
       <h2>Analytics Setup</h2>
       <p>An application to test Analytics setup with Analytics Tracking API</p>
+      {analyticsData && <p>{analyticsData}</p>}
     </div>
   );
 };
