313d956c5337cfcb36bff853df142d78f89bc703
Okoro John Darlington <joniedev101@gmail.com>
2024-04-03 09:32:25 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,38 +2,38 @@ import { useEffect, useState } from "react";
 import ReactGA from "react-ga4";
 
 const App = () => {
-  const [analyticsData, setAnalyticsData] = useState(null);
+  // const [analyticsData, setAnalyticsData] = useState(null);
 
-  useEffect(() => {
-    ReactGA.initialize("G-08SGRJZ6RY");
+  // useEffect(() => {
+  //   ReactGA.initialize("G-08SGRJZ6RY");
 
-    ReactGA.send("pageview", {
-      page: window.location.pathname + window.location.search,
-    });
+  //   ReactGA.send("pageview", {
+  //     page: window.location.pathname + window.location.search,
+  //   });
 
-    const data = ReactGA.ga("getData");
-    setAnalyticsData(data);
-  }, []);
+  //   const data = ReactGA.ga("getData");
+  //   setAnalyticsData(data);
+  // }, []);
 
-  useEffect(() => {
-    ReactGA.event({
-      category: "Category name",
-      action: "Action name",
-    });
+  // useEffect(() => {
+  //   ReactGA.event({
+  //     category: "Category name",
+  //     action: "Action name",
+  //   });
 
-    ReactGA.send("pageview", { page: "/" });
+  //   ReactGA.send("pageview", { page: "/" });
 
-    const data = ReactGA.ga("getAll");
-    setAnalyticsData(data);
-  }, []);
+  //   const data = ReactGA.ga("getAll");
+  //   setAnalyticsData(data);
+  // }, []);
 
-  console.log(analyticsData);
+  // console.log(analyticsData);
 
   return (
     <div className="app">
       <h2>Analytics Setup</h2>
       <p>An application to test Analytics setup with Analytics Tracking API</p>
-      {analyticsData && <p>{analyticsData}</p>}
+      {/* {analyticsData && <p>{analyticsData}</p>} */}
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-// import ReactGA from "react-ga4";
+import ReactGA from "react-ga4";
 
-// ReactGA.initialize("G-08SGRJZ6RY");
+ReactGA.initialize("G-08SGRJZ6RY");
 
-// ReactGA.send({
-//   hitType: "pageview",
-//   page: window.location.pathname,
-//   title: "Custom Title",
-// });
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+  title: "Custom Title",
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
