9c21bbbfac529733451b8703093dd07bea3e585b
Okoro John Darlington <joniedev101@gmail.com>
2024-04-03 07:18:57 +0100

    Commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="app">
+      <h2>Analytics Setup</h2>
+      <p>An application to test Analytics setup with Analytics Tracking API</p>
+    </div>
+  );
+}
+
+export default App;
