ba056b4dd39f5fd1db3dcd0717e01854f1d8c910
Okoro John Darlington <joniedev101@gmail.com>
2024-04-03 07:47:55 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-function App() {
+const App = () => {
   return (
     <div className="app">
       <h2>Analytics Setup</h2>
       <p>An application to test Analytics setup with Analytics Tracking API</p>
     </div>
   );
-}
+};
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-08SGRJZ6RY");
+
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+  title: "Custom Title",
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
